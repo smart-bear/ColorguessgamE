97f0c8e3f15409e20838e3bb872b6712ef7cb4d6
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-04-11 17:12:32 +0000

    assertion

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -5,4 +5,7 @@
  * @prev: previous block
  * @data: data for a new block
  * @data_len: length of @data
- * Return: a block or NULL if fa
+ * Return: a block or NULL if failed
+ */
+block_t *block_create(block_t const *prev,
+		
