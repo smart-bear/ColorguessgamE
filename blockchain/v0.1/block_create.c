ebcbdbeb25c44c8eb456f9c1b667bbc7494c1d78
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-04-18 12:28:31 +0000

    Update nautical-charts.html

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -11,4 +11,6 @@ block_t *block_create(block_t const *prev,
 		      int8_t const *data, uint32_t data_len)
 {
 block_t *block;
-time_t t
+time_t t;
+int i;
+uint32_t max_len = data_len > BLOCKCHAIN_DATA_MAX ?
