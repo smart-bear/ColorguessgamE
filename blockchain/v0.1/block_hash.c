3695945ca6158cac36e57cd270e122d11ffb0209
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-05-18 15:50:44 +0000

    deleted some files

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,4 +1,7 @@
 #include "blockchain.h"
 
 uint8_t *block_hash(block_t const *block,
-		    uint8_t hash_buf[SHA256_
+		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+memset(hash_buf, 0, SHA256_DIGEST_LENGTH);
+retu
