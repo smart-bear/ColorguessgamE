d0f72df465dacae3d072ee792656eb3ccd36e323
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-05-23 14:36:34 +0000

    s390: move dummy io_remap_pfn_range() to asm/pgtable.h

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -4,4 +4,5 @@ uint8_t *block_hash(block_t const *block,
 		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
 memset(hash_buf, 0, SHA256_DIGEST_LENGTH);
-retu
+return (SHA256((const unsigned char *)block,
+	       sizeof(block->i
