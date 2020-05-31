9768b448312ecfcaedcc5972e952dcbff640344c
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-05-31 18:39:08 +0000

    0.10.5

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -2,4 +2,5 @@
 
 /**
  * block_is_valid - checks if this and previous block are valid
- * @block: pointer to this block
+ * @block: pointer to this block in the chain
+ * @prev_block: pointer to previous block in 
