7321c292d6fb36671c0e2f1d4d5b5eeb02d613e1
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-05-31 21:31:49 +0000

    minor whitespace cleanup

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -4,4 +4,6 @@
  * block_is_valid - checks if this and previous block are valid
  * @block: pointer to this block in the chain
  * @prev_block: pointer to previous block in the chain or NULL
- * Return: 0 if valid else 1 if invali
+ * Return: 0 if valid else 1 if invalid
+ */
+int block_is_valid(block_t const *block, block_t 
