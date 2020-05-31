322b86fece2d371075e9344712cddf58e59c8b12
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-05-31 19:10:27 +0000

    Automated master to rc merge

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -3,4 +3,5 @@
 /**
  * block_is_valid - checks if this and previous block are valid
  * @block: pointer to this block in the chain
- * @prev_block: pointer to previous block in 
+ * @prev_block: pointer to previous block in the chain or NULL
+ * Return: 0 if valid else 1 if invali
