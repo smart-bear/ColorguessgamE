d9640978c66b58f7cf4e728aeba36a96b0ab2337
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-06-06 07:49:54 +0000

    bone

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -6,4 +6,6 @@
  * @prev_block: pointer to previous block in the chain or NULL
  * Return: 0 if valid else 1 if invalid
  */
-int block_is_valid(block_t const *block, block_t 
+int block_is_valid(block_t const *block, block_t const *prev_block)
+{
+uint8_t hash_buf[SHA256_DIGEST_LENGT
