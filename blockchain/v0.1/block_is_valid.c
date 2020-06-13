5bac02cb161882b2c0c1a722496ad5c95c813ead
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-06-13 10:33:42 +0000

    Respond to feedback

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -11,4 +11,6 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 uint8_t hash_buf[SHA256_DIGEST_LENGTH] = {0};
 block_t const _genesis = GENESIS_BLOCK;
 if (!block || (!prev_block && block->info.index != 0))
-return
+return (1);
+if (block->info.index == 0)
+return (memcmp(block, &_genesis, si
