00bf3abbf3ac6d70efde8086dcd0d262c54d8329
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-06-13 22:02:54 +0000

    Move some code around

--- blockchain/v0.1/block_is_valid.c
+++ blockchain/v0.1/block_is_valid.c
@@ -16,4 +16,6 @@ if (block->info.index == 0)
 return (memcmp(block, &_genesis, sizeof(_genesis)));
 if (block->info.index != prev_block->info.index + 1)
 return (1);
-if (!block_hash(prev_block, 
+if (!block_hash(prev_block, hash_buf) ||
+memcmp(hash_buf, prev_block->hash, SHA256_DIGEST_LENGTH))
+r
