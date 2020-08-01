aeab59cc9284911d39e9020a954b01b71f1ba30d
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-08-01 20:45:02 +0000

    Documenting

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -72,4 +72,4 @@ void block_destroy(block_t *block);
 
 void blockchain_destroy(blockchain_t *blockchain);
 
-uint8_t *block_hash
+uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_D
