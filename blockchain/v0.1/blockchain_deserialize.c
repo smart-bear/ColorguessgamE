1c3fff34f91c1149e981957fb8a75b0137d05d8b
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-08-20 09:39:17 +0000

    updated test data

--- blockchain/v0.1/blockchain_deserialize.c
+++ blockchain/v0.1/blockchain_deserialize.c
@@ -4,4 +4,6 @@
  * convert_endianness - Convert data of a block
  * @block: block to be converted
  */
-void convert_endianness(block_t *b
+void convert_endianness(block_t *block)
+{
+_swap_endian(&block->info.index
