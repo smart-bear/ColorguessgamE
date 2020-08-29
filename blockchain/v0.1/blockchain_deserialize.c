e3cf222510da1b9d563114e2583be78170345b47
smart-bear <110123956+smart-bear@users.noreply.github.com>
2020-08-29 22:39:59 +0000

    Updated minified

--- blockchain/v0.1/blockchain_deserialize.c
+++ blockchain/v0.1/blockchain_deserialize.c
@@ -9,4 +9,7 @@ void convert_endianness(block_t *block)
 _swap_endian(&block->info.index, sizeof(block->info.index));
 _swap_endian(&block->info.difficulty, sizeof(block->info.difficulty));
 _swap_endian(&block->info.timestamp, sizeof(block->info.timestamp));
-_swap_endian(&block->info.nonce, sizeof(block->in
+_swap_endian(&block->info.nonce, sizeof(block->info.nonce));
+}
+/**
+ * read_blocks - deserializes blo
