23d5af3be436774161cc7bd77554dd8fe1771743
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-01-10 11:28:49 +0000

    Amend readme

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -3,4 +3,5 @@
 /**
  * write_block - a function that serializes a Blockchain into a file
  * @ptr: pointer to the block to be serialized
- * @idx: in
+ * @idx: index of the parameter
+ * @arg: The file in which the data will be wri
