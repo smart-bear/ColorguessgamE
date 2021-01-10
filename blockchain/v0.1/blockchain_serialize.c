edec7d5a9a24a22a6b840f6688dbab1f6e7a24ad
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-01-10 10:24:39 +0000

    submit button

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -1,4 +1,6 @@
 #include "blockchain.h"
 
 /**
- * write_block - a function that serializes a Blockchain into a 
+ * write_block - a function that serializes a Blockchain into a file
+ * @ptr: pointer to the block to be serialized
+ * @idx: in
