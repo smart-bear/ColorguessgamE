06e8fccecc6996c4a1d6bdd92baf1f7573551db7
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-02-17 07:49:16 +0000

    Changed appveyor.yml

--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -15,4 +15,6 @@ UNUSED(idx);
 fp = (FILE *)arg;
 fwrite((void *)&block->info, sizeof(block->info), 1, fp);
 fwrite((void *)&block->data.len, sizeof(block->data.len), 1, fp);
-fwrite(block->data.buffer, block->data.len, 1,
+fwrite(block->data.buffer, block->data.len, 1, fp);
+fwrite(block->hash, sizeof(block->hash), 1, fp);
+return
