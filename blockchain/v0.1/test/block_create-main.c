d43eadffe7e4739567a8238317c4294969140dff
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-05-15 20:57:44 +0000

    arm: use -mtune=cortex-a15 for Krait targets

--- blockchain/v0.1/test/block_create-main.c
+++ blockchain/v0.1/test/block_create-main.c
@@ -22,4 +22,5 @@ block = llist_get_head(blockchain->chain);
 
 block = block_create(block, (int8_t *)"Holberton", 9);
 llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
-block = block_create(block, (
+block = block_create(block, (int8_t *)"School", 6);
+llist_add_node(blockchain->chain, block, 
