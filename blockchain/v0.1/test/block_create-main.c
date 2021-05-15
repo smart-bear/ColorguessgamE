8f3ee7ae44a7cc00d208e51317bf1f16146cf551
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-05-15 07:18:21 +0000

    sched: Make sure to not re-read variables after validation

--- blockchain/v0.1/test/block_create-main.c
+++ blockchain/v0.1/test/block_create-main.c
@@ -5,4 +5,9 @@
 #include "blockchain.h"
 
 void _blockchain_print(blockchain_t const *blockchain);
-void _blockchain_destroy(b
+void _blockchain_destroy(blockchain_t *blockchain);
+
+/**
+ * main - Entry point
+ *
+ * Re
