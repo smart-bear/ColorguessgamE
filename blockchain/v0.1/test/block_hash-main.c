2c89161f73140d6177da67c7bc004c6cf5f7916f
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-05-30 19:08:15 +0000

    modify the README.md

--- blockchain/v0.1/test/block_hash-main.c
+++ blockchain/v0.1/test/block_hash-main.c
@@ -20,3 +20,16 @@ block_t *block;
 
 blockchain = blockchain_create();
 block = llist_get_head(blockchain->chain);
+
+block = block_create(block, (int8_t *)"Holberton", 9);
+llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+_blockchain_print(blockchain);
+
+block_hash(block, block->hash);
+block = block_create(block, (int8_t *)"School", 6);
+llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+_blockchain_print(blockchain);
+
+_blockchain_destroy(blockchain);
+return (EXIT_SUCCESS);
+}
