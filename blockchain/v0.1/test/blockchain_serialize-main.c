e50060ca0317330c856d09fdb07969ef8dea0a6d
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-09-14 07:18:38 +0000

    Release 1.0.10

--- blockchain/v0.1/test/blockchain_serialize-main.c
+++ blockchain/v0.1/test/blockchain_serialize-main.c
@@ -22,4 +22,5 @@ block_hash(block, block->hash);
 llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
 block = block_create(block, (int8_t *)"School", 6);
 block_hash(block, block->hash);
-llist_add
+llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+block = b
