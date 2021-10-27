5886082fdfc1d3120f9d4041fc61157790f5cb9a
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-10-27 13:36:00 +0000

    compile fixes

---
+++ blockchain/v0.2/block_create.c
@@ -0,0 +1,22 @@
+
+#include "blockchain.h"
+#include <time.h>
+/**
+ * block_create - creates a new block
+ * @prev: previous block
+ * @data: data for a new block
+ * @data_len: length of @data
+ * Return: a block or NULL if failed
+ */
+block_t *block_create(block_t const *prev,
+		      int8_t const *data, uint32_t data_len)
+{
+block_t *block;
+time_t t;
+int i;
+uint32_t max_len = data_len > BLOCKCHAIN_DATA_MAX ?
+BLOCKCHAIN_DATA_MAX : data_len;
+if (prev == NULL)
+return (NULL);
+if (data == NULL)
+return (NULL);
