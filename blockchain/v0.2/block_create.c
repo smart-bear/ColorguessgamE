ea42a25cf7f6464e7a030673caa95a379f0430f7
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-10-28 11:13:41 +0000

    starting documentation

--- blockchain/v0.2/block_create.c
+++ blockchain/v0.2/block_create.c
@@ -20,3 +20,25 @@ if (prev == NULL)
 return (NULL);
 if (data == NULL)
 return (NULL);
+block = (block_t *)malloc(sizeof(block_t));
+block->info.index = (uint32_t)((int)(prev->info.index) + 1);
+block->info.difficulty = 0;
+t = time(0);
+block->info.timestamp = (uint64_t)&t;
+block->info.nonce = 0;
+for (i = 0; i < BLOCKCHAIN_DATA_MAX; i++)
+{
+block->data.buffer[i] = 0;
+}
+for (i = 0; i < (int)max_len; i++)
+{
+block->data.buffer[i] = data[i];
+}
+block->data.len = max_len;
+for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
+{
+block->hash[i] = 0;
+block->info.prev_hash[i] = prev->hash[i];
+}
+return (block);
+}
---
+++ blockchain/v0.2/block_hash.c
@@ -0,0 +1,10 @@
+
+#include "blockchain.h"
+
+uint8_t *block_hash(block_t const *block,
+		    uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+{
+memset(hash_buf, 0, SHA256_DIGEST_LENGTH);
+return (SHA256((const unsigned char *)block,
+	       sizeof(block->info) + block->data.len, hash_buf));
+}
