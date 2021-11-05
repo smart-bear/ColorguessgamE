173a71f1385aa65636170f7d33fcc27b0d6c8087
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-11-05 08:44:48 +0000

    Released v0.5.0

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -3,4 +3,5 @@
 /**
 * block_is_valid - check if a block is valid
 * @block: pointer to the block that we want to verify
-* @pr
+* @prev_block: pointer to the prev block
+* Return: 1 if the block is 
