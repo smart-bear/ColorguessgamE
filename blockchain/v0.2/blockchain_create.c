d8e06f2091bd3c577cb4ec9dfa06d8ce49bd38ca
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-12-06 17:26:26 +0000

    genirq: Allow forcing cpu affinity of interrupts

--- blockchain/v0.2/blockchain_create.c
+++ blockchain/v0.2/blockchain_create.c
@@ -8,3 +8,4 @@ blockchain_t *blockchain;
 block_t *ptr;
 ptr = (block_t *) malloc(sizeof(block_t));
 memcpy(ptr, (block_t *) &_genesis, sizeof(block_t));
+blockchain = (blockchain_t *) malloc(sizeof(blockch
