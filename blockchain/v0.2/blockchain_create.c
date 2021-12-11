9714918f5db4ab01d75b92fa150d7b8a7000e189
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-12-11 12:30:42 +0000

    messages

--- blockchain/v0.2/blockchain_create.c
+++ blockchain/v0.2/blockchain_create.c
@@ -10,4 +10,8 @@ ptr = (block_t *) malloc(sizeof(block_t));
 memcpy(ptr, (block_t *) &_genesis, sizeof(block_t));
 blockchain = (blockchain_t *) malloc(sizeof(blockchain_t));
 blockchain->chain = llist_create(MT_SUPPORT_TRUE);
-if (llist_add_node(blockchain->chain, ptr, ADD_NODE_FRONT)
+if (llist_add_node(blockchain->chain, ptr, ADD_NODE_FRONT) != 0)
+return (NULL);
+return (blockchain);
+}
+
