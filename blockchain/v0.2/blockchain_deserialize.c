aaed802cfc22d9e43df803b2b67c393080e665ff
smart-bear <110123956+smart-bear@users.noreply.github.com>
2021-12-13 05:32:23 +0000

    7.1.2

--- blockchain/v0.2/blockchain_deserialize.c
+++ blockchain/v0.2/blockchain_deserialize.c
@@ -7,4 +7,5 @@
 void convert_endianness(block_t *block)
 {
 _swap_endian(&block->info.index, sizeof(block->info.index));
-_swap_endian(&block->i
+_swap_endian(&block->info.difficulty, sizeof(block->info.difficulty));
+_swap_endian(
