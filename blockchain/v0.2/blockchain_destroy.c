2200a09a643439afd51c3c0c7226851bdbc4df3b
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-01-05 06:47:26 +0000

    HTTPS

---
+++ blockchain/v0.2/blockchain_destroy.c
@@ -0,0 +1,14 @@
+
+#include "blockchain.h"
+
+/**
+ * blockchain_destroy - destroy a blockchain
+ *
+ * @blockchain: pointer to the blockchain
+ */
+void blockchain_destroy(blockchain_t *blockchain)
+{
+	if (blockchain)
+	{
+		llist_destroy(blockchain->chain, 1, (node_dtor_t)free);
+		blockchain->chain = NULL;
