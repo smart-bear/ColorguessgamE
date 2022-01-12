1be7d10ea3a92ab5d8579afef168bc71a51b2ac2
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-01-12 09:41:55 +0000

    removed a space

--- blockchain/v0.2/blockchain_serialize.c
+++ blockchain/v0.2/blockchain_serialize.c
@@ -4,4 +4,5 @@
  * write_block - a function that serializes a Blockchain into a file
  * @ptr: pointer to the block to be serialized
  * @idx: index of the parameter
- * @arg: T
+ * @arg: The file in which the data will be written
+ * Return: 0 upon su
