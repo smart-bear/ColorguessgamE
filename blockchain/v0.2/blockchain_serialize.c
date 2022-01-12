951ffcbbb60e2d1e9be840c319576ad5993ae6f3
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-01-12 07:01:47 +0000

    - log

--- blockchain/v0.2/blockchain_serialize.c
+++ blockchain/v0.2/blockchain_serialize.c
@@ -2,4 +2,6 @@
 
 /**
  * write_block - a function that serializes a Blockchain into a file
- * @ptr: pointer to the block to 
+ * @ptr: pointer to the block to be serialized
+ * @idx: index of the parameter
+ * @arg: T
