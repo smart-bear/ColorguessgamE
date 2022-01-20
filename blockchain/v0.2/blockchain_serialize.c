c6aeca7ae5a010446e16c37dd3beff03eab1e6e2
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-01-20 12:29:30 +0000

    sctp: avoid BUG_ON on sctp_wait_for_sndbuf

--- blockchain/v0.2/blockchain_serialize.c
+++ blockchain/v0.2/blockchain_serialize.c
@@ -14,4 +14,5 @@ block_t *block = ptr;
 UNUSED(idx);
 fp = (FILE *)arg;
 fwrite((void *)&block->info, sizeof(block->info), 1, fp);
-fwrite((void *)&block->data.
+fwrite((void *)&block->data.len, sizeof(block->data.len), 1, fp);
+fwrite(block-
