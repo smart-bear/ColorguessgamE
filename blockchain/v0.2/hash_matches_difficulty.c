3516193f46b3bd16507176e787b6b1c4c2426eb8
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-02-12 12:15:17 +0000

    Publish

--- blockchain/v0.2/hash_matches_difficulty.c
+++ blockchain/v0.2/hash_matches_difficulty.c
@@ -6,4 +6,7 @@
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
 uint32_t difficulty)
 {
-int i = 0, bit = 0, one_found = 0, copy_di
+int i = 0, bit = 0, one_found = 0, copy_difficulty = 0;
+
+copy_difficulty = difficulty;
+for (i = 
