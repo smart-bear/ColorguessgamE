b055798b906503ff8ba3f15a7b4b9e46604cd27b
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-03-02 18:46:08 +0000

    Tweak the README

--- blockchain/v0.2/provided/_endianness.h
+++ blockchain/v0.2/provided/_endianness.h
@@ -8,4 +8,6 @@
  * # include <stdint.h>
  *
  * # define SWAPENDIAN_16(value)	(((value & 0xFF00) >> 8) | \
- *				((value & 0x00FF) << 8
+ *				((value & 0x00FF) << 8))
+ *
+ * # define SWAPENDIAN_32(value)	(((value & 0x
