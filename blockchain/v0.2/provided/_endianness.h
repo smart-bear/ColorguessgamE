2713ba6497ac684243be80de03350ba8b044374b
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-03-05 18:37:11 +0000

    Add a .gitignore file.

--- blockchain/v0.2/provided/_endianness.h
+++ blockchain/v0.2/provided/_endianness.h
@@ -23,4 +23,6 @@
  *				((value & 0x0000000000FF0000ULL) << 24) | \
  *				((value & 0x000000000000FF00ULL) << 40) | \
  *				((value & 0x00000000000000FFULL) << 56))
- *
+ */
+
+# define SWAPENDIAN(value)	_swap_endian((char *)(&value), sizeo
