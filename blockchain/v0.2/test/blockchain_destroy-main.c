d94ff6d067d63fd228ac3c4859a6fdc12e3ff9ac
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-04-16 15:34:33 +0000

    ip_forward: Drop frames with attached skb->sk

--- blockchain/v0.2/test/blockchain_destroy-main.c
+++ blockchain/v0.2/test/blockchain_destroy-main.c
@@ -21,4 +21,5 @@ block = block_create(block, (int8_t *)"Holberton", 9);
 llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
 block = block_create(block, (int8_t *)"School", 6);
 llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
-block = block_create(block, 
+block = block_create(block, (int8_t *)"of", 2);
+llist_add_node(blockchain->chain, block
