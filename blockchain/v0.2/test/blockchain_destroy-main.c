6c54645a3a913cffdb41c3531d3e89a8bba3fe9f
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-04-19 17:08:03 +0000

    [MERGE] sync with trunk

--- blockchain/v0.2/test/blockchain_destroy-main.c
+++ blockchain/v0.2/test/blockchain_destroy-main.c
@@ -25,4 +25,5 @@ block = block_create(block, (int8_t *)"of", 2);
 llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
 block = block_create(block, (int8_t *)"Software", 8);
 llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
-block
+block = block_create(block, (int8_t *)"Engineering", 11);
+llist_add_no
