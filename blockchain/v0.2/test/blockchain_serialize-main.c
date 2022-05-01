143a6ec1b1fa49aa341b6ad81150021cffa7b0d3
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-05-01 08:57:29 +0000

    Forgot one.

--- blockchain/v0.2/test/blockchain_serialize-main.c
+++ blockchain/v0.2/test/blockchain_serialize-main.c
@@ -25,4 +25,5 @@ block_hash(block, block->hash);
 llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
 block = block_create(block, (int8_t *)"of", 2);
 block_hash(block, block->hash);
-llist_add_node(
+llist_add_node(blockchain->chain, block, ADD_NODE_REAR);
+block = block_create(bloc
