5a346002b2e6149f8c0ef16599a569fd38782c12
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-07-06 21:54:29 +0000

    Conventions

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -4,4 +4,6 @@
  * ec_from_pub - create an EC_KEY structure from a public key
  * @pub: public key to be converted
  *
- * Return: pointer to created EC_KEY, 
+ * Return: pointer to created EC_KEY, NULL on error
+ */
+EC_KEY *ec_from_pub(uint8_t const pub[EC
