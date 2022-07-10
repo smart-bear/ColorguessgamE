ad87f8ae1f6399e3924419b4fceaa129c1dcfa77
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-07-10 09:31:23 +0000

    clean up some warnings

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -21,4 +21,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	p = EC_POINT_new(group);
 	if (!p)
 	{
-		EC_KEY_free
+		EC_KEY_free(key);
+		return (NULL);
+	}
+	if (!EC_POINT_oct2point(group, p, pub, E
