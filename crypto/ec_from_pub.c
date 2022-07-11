316ede2831c8dba738985941bbe7c7e82c287b6e
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-07-11 18:01:44 +0000

    Développeur MySql (H/F) en CDI à Paris

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -26,4 +26,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	}
 	if (!EC_POINT_oct2point(group, p, pub, EC_PUB_LEN, NULL))
 		goto out;
-	if (!EC_KEY_set_public_key(key
+	if (!EC_KEY_set_public_key(key, p))
+		goto out;
+	EC_POINT_free(p);
+	return (key)
