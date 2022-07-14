71a8c1c753dbcab9ce06676ecf5d1023d1845e37
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-07-14 06:34:19 +0000

    Update androidlinks.txt

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -29,4 +29,10 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 	if (!EC_KEY_set_public_key(key, p))
 		goto out;
 	EC_POINT_free(p);
-	return (key)
+	return (key);
+out:
+	EC_KEY_free(key);
+	EC_POINT_free(p);
+	return (NULL);
+}
+
