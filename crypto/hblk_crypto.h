cb08588749f8e88ada2fc7faab570e60fdfa617b
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-11-11 20:38:29 +0000

    Correct comment.

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -44,4 +44,5 @@ uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
 size_t msglen, sig_t *sig);
 int ec_verify(EC_KEY const *key, uint8_t const *msg,
 size_t msglen, sig_t const *sig);
-#endif /* H
+#endif /* HBLK_CRYPTO_H */
+
