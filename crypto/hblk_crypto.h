d3f389ed49867a1c5d876f58becc936fc69061d2
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-11-11 14:06:37 +0000

    javadoc fix

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -40,4 +40,6 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
 int ec_save(EC_KEY *key, char const *folder);
 EC_KEY *ec_load(char const *folder);
-uint8_t *ec_sign(EC_KEY const *key, uint8_t const *ms
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+size_t msglen, sig_t *sig);
+int ec_verify(EC_KEY const 
