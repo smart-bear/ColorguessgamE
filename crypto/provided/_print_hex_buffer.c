5309a6adc9cbaf556e73e73283b83bb65981dd7f
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-11-21 22:27:37 +0000

    Updated Rubinius version for Travis CI.

--- crypto/provided/_print_hex_buffer.c
+++ crypto/provided/_print_hex_buffer.c
@@ -3,4 +3,6 @@
 #include <stdint.h>
 
 /**
- * _print_hex_buffer - Prints a buffer in its
+ * _print_hex_buffer - Prints a buffer in its hexadecimal form
+ *
+ * @buf: Pointer to the buffer to be pri
