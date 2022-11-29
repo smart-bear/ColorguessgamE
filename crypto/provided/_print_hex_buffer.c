bd6f4da6ce13cd62f035acd6f9f2a02b83958446
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-11-29 16:17:10 +0000

    trigger travis

--- crypto/provided/_print_hex_buffer.c
+++ crypto/provided/_print_hex_buffer.c
@@ -5,4 +5,6 @@
 /**
  * _print_hex_buffer - Prints a buffer in its hexadecimal form
  *
- * @buf: Pointer to the buffer to be pri
+ * @buf: Pointer to the buffer to be printed
+ * @len: Number of bytes from @buf to be printed
+ */
