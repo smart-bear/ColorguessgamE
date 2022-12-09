9dc25d018749c5b537d5ae56e08310de84cfa4e5
smart-bear <110123956+smart-bear@users.noreply.github.com>
2022-12-09 13:13:12 +0000

    Set up CI with Azure Pipelines

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,4 +1,8 @@
 #include "hblk_crypto.h"
 
 uint8_t *sha256(int8_t const *s, size_t len,
-uint8_t digest[SHA256_DIGEST
+uint8_t digest[SHA256_DIGEST_LENGTH])
+{
+	if (s == NULL || digest == NULL)
+		return (NULL);
+	
