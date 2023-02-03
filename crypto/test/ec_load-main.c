192f6016d6aef028c2b188ca96698527c98839c6
smart-bear <110123956+smart-bear@users.noreply.github.com>
2023-02-03 11:57:27 +0000

    Fixed logger

--- crypto/test/ec_load-main.c
+++ crypto/test/ec_load-main.c
@@ -11,4 +11,6 @@ void _print_hex_buffer(uint8_t const *buf, size_t len);
  * @ac: Arguments count
  * @av: Arguments vector
  *
- * Return: EXIT_SUCCESS o
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(int ac, char **av
