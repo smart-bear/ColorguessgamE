8bdc4cb7c00928b9fd8e46f9a61152e607e88f09
smart-bear <110123956+smart-bear@users.noreply.github.com>
2023-03-18 20:44:48 +0000

    updated gradle wrapper

--- crypto/test/ec_verify-main.c
+++ crypto/test/ec_verify-main.c
@@ -4,4 +4,8 @@
 
 #include "hblk_crypto.h"
 
-void _print_hex_buffer(uint8_t const *buf, size_t 
+void _print_hex_buffer(uint8_t const *buf, size_t len);
+
+/**
+ * test_ec_sign - Test the ec_sign function
+ 
