007e2da69e8a58e45b71dbaf6a6dd255bf96e5a9
smart-bear <110123956+smart-bear@users.noreply.github.com>
2023-03-19 09:14:54 +0000

    Update Login.php

--- crypto/test/ec_verify-main.c
+++ crypto/test/ec_verify-main.c
@@ -9,4 +9,8 @@ void _print_hex_buffer(uint8_t const *buf, size_t len);
 /**
  * test_ec_sign - Test the ec_sign function
  *
- * @key: Pointer to the EC Key pair to use to sign the 
+ * @key: Pointer to the EC Key pair to use to sign the message
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+static int test
