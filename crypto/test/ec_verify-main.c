24fa5a1af38f48ef2ba3d107beeca496843dc417
smart-bear <110123956+smart-bear@users.noreply.github.com>
2023-03-27 13:09:55 +0000

    Updated summary.

--- crypto/test/ec_verify-main.c
+++ crypto/test/ec_verify-main.c
@@ -64,4 +64,10 @@ return (EXIT_FAILURE);
 if (test_ec_sign(key) != EXIT_SUCCESS)
 {
 EC_KEY_free(key);
-return (EXIT_FAI
+return (EXIT_FAILURE);
+}
+
+    /* Cleanup */
+EC_KEY_free(key);
+
+return (EXIT_
